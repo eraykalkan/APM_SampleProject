6c75d83c827e6f9479fa11a5ae904b818fbb1ea2
eraykalkan <eraykalkan@gmail.com>
2020-11-08 16:19:56 +0300

    Update product-list.component.ts
    Add onInit implementation

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import{ IProduct } from './product'
 
 @Component({
@@ -7,7 +7,10 @@ import{ IProduct } from './product'
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+    ngOnInit(): void {
+        console.log('In OnInit');
+    }
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number= 2;
