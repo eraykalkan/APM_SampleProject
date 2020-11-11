2669292dbd128eeafb5db362491779f8c203c6fc
eraykalkan <eraykalkan@gmail.com>
2020-11-11 22:36:22 +0300

    Add product-detail-guard

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
 import { IProduct } from './product';
 
 @Component({
@@ -9,9 +12,27 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string= 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,private router: Router) {
+
+   }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId' : id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2019',
+      'description': 'asdsadas',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'asdasdas'
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
